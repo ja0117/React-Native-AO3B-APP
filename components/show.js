600da45807b34f1871a5336f2cd60ac25a4ebeb9
Sven Tjeerdsma <sven-tjeerd-sma@hotmail.com>
2018-10-08 10:44:35 +0200

    added structure

---
+++ components/show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Show extends Component {
+    render() {
+        return (
+            <View>
+               test 
+            </View>
+        );
+    }
+}
+
+export default Show;
