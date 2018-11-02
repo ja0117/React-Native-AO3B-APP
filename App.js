61b36e9eb9a09cad73a210fcaf111731c1ad5559
Jelle Huiberts <jellio.huiberts@gmail.com>
2018-11-02 12:58:01 +0100

    added img

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View,Dimensions } from 'react-native';
+import { StyleSheet, Text, View,Dimensions, Image } from 'react-native';
 import Chart from './components/Chart';
 import Graph from './components/Graph';
 import Footer from './components/Footer';
 
-var {height, width} = Dimensions.get('window');
+var {height, width} = Dimensions.get('window'); //REEEEEEEEEEEEEEEEEEE
 export default class App extends React.Component {
   constructor(props)
   {
@@ -17,9 +17,16 @@ export default class App extends React.Component {
     console.log(height);
     return (
       <View style={styles.container}>
-         <View style={styles.title}><Text>titel</Text></View>
+         <View style={styles.title}><Text style={styles.text}>SensHagen</Text>
+         <Image
+           style={{width: 50, height: 50}}
+            source={require('../React-Native-AO3B-APP/images/zwolle.jpg')}
+          />
+         </View>
          <View><Chart /></View>
-         <View><Text>waarde 1</Text></View>
+         <View>
+           <Text>waarde 1</Text>
+         </View>
          <View></View>
          <View><Graph /></View> 
          <View><Footer authors="sven" /></View>
@@ -30,16 +37,22 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
 
-  // title: {
-  //   display: 'flex',
-  //   flex: 11,
-  //   // backgroundColor: 'black',
-  // },
-  // container: {
-  //   top: 25,
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   // alignItems: 'center',
-  //   // justifyContent: 'center',
-  // },
+  title: {
+    backgroundColor: 'white',
+    width: width,
+    padding: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  text: {
+    fontSize: 50,
+    color: "#476DB7",
+  },
+  container: {
+    backgroundColor: 'red',
+    top: 25,
+    flex: 12,
+    alignItems: 'center',
+  },
 });
