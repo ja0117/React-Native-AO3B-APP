9d8dcf94e56cb2c1dec302a84a1b9e57f892d87d
Mike de Weert <97041937@st.deltion.nl>
2018-11-20 10:57:04 +0100

    Added Graph to master

--- components/Graph.js
+++ components/Graph.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View,Dimensions } from 'react-native';
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
+
 class Graph extends Component {
     render() {
         return (
             <View>
+    render() 
+    {
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
 
             </View>
         );
+        return (
+            <AreaChart
+                style={{ height: 200 }}
+                data={ data }
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={ shape.curveNatural }
+                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+            >
+                <Grid/>
+            </AreaChart>
+        )
     }
 }
 
