e469c1078857b18965b9bf2647f61f063daf77f5
Mike de Weert <97041937@st.deltion.nl>
2018-11-20 10:34:59 +0100

    Added graph
    CHART

--- components/Graph.js
+++ components/Graph.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View,Dimensions } from 'react-native';
-class Graph extends Component {
-    render() {
-        return (
-            <View>
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 
-            </View>
-        );
+class Graph extends Component {
+    render() 
+    {
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        return (
+            <AreaChart
+                style={{ height: 200 }}
+                data={ data }
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={ shape.curveNatural }
+                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+            >
+                <Grid/>
+            </AreaChart>
+        )
     }
 }
-
 
 export default Graph;
