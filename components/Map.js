35a29fd550b75ca9d5f8ae84a5409295b8073b53
Joshua <joshua007.joshua@gmail.com>
2018-12-19 23:34:05 +0100

    rip skyblock.nl. map.js krijgt geen data binnen. To Do: State vullen met Data + Await gebruiken

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,76 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import Axios from 'axios';
+
+
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
 
 class Map extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null,
+            markers: [],
+        }
+    }
+
+    fetchData() {
+
+        Axios.get('http://skyblock.nl/api/app/data/sensorlocation')
+            .then(response => { 
+
+                var data = [];
+
+                for(let i = 0; i < response.data.length; i ++ ){
+
+                    if(!response.data[i] == null)
+                        continue;
+
+                    data.push(response.data[i]);
+                }
+
+                this.state.markers = data;
+                return data;
+
+            }).catch(error => {
+                console.log(error);
+            });
+        
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+
+                console.log("Woookieees");
+                console.log(position);
+
+                this.setState ({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+
+            },
+            (error) => this.setState({error: error.message}),
+            {enableHighAccuracy: true, timeout: 20000}
+        );
+
+        console.log(this.fetchData());
+    }
+
     render() {
         return (
-            <MapView 
-                style = {{ flex: 1 }}
-                initialRegion= {{
-                    latitude: 52.5351226,
-                    longitude: 6.0453339,
-                    latitudeDelta: 0.009,
-                    longitudeDelta: 0.009
-                }}
+            <MapView
+                style = {{flex: 1}}
+                initialRegion={this.state.initialPosition}
             />
+            
         );
     }
 }
